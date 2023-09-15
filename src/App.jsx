e9dc8ce4629cc2ba19778ace8d243b792c2dea70
UniquePG <guptapavan549@gmail.com>
2023-09-15 22:20:40 +0530

    added denied page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CourseCard from './Components/CourseCard'
 import AboutUs from './Pages/AboutUs'
 import ContactUs from './Pages/ContactUs'
 import CourseList from './Pages/Courses/CourseList'
+import Denied from './Pages/Denied'
 import Homepage from './Pages/Homepage'
 import Login from './Pages/Login'
 import NotFound from './Pages/NotFound'
@@ -20,12 +21,15 @@ function App() {
       <Route path="/about"  element={<AboutUs />} />
       <Route path='/signup' element={<Signup />} /> 
       <Route path='/login' element={<Login />} /> 
+      
 
       <Route path='/courses' element={<CourseList />} />
       <Route path='/contact' element={<ContactUs />} />
       <Route path='/card' element={<CourseCard />} /> 
 
 
+    {/* if you not have access of a page */}
+      <Route path='/denied' element={<Denied />} /> 
 
       {/* If any rount not exists Page not found */}
       <Route path='*' element={<NotFound />} />
---
+++ src/Pages/Denied.jsx
@@ -0,0 +1,28 @@
+import { Navigate, useNavigate } from "react-router-dom"
+
+const Denied = () => {
+
+    const navigate = useNavigate();
+
+  return (
+    <main className="flex flex-col w-full h-screen justify-center items-center bg-[#1A2238]">
+        <h1 className="text-9xl font-extrabold tracking-widest text-white">
+            403
+        </h1>
+
+        <div className="bg-black text-white text-sm rounded rotate-12 absolute">
+            Access Denied
+        </div>
+
+        <button className="mt-5" 
+                onClick={()=> navigate(-1)} >
+            <span className="px-8 py-2 bg-[#1A2238] relative border border-current text-white">
+                Go Back
+            </span>
+        </button>
+        
+    </main>
+  )
+}
+
+export default Denied
